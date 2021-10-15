aa4a9ab7cae4eeb177421372ebfe85b59721b418
Anousone-FS <anousone.litd@gmail.com>
2021-10-15 17:15:12 +0700

    create sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,65 @@
 import React from "react"
+import { TrendingUp } from "@mui/icons-material"
+import "./Sidebar.css"
 
 const SideBar: React.FC = () => {
-    return <div className="sidebar">sidebar</div>
+    return (
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <div className="sidebarMenu">
+                    <span className="sidebarTitle">home</span>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <span className="sidebarTitle">home</span>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="sidebarMenu">
+                    <span className="sidebarTitle">home</span>
+                    <ul className="sidebarList">
+                        <li className="sidebarListItem active">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                        <li className="sidebarListItem">
+                            <TrendingUp className="icon" />
+                            <span>trending</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default SideBar
